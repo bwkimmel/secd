7ad677220f39eb2d4136babf92a31f73ef265d85
Brad Kimmel <intrepidca@gmail.com>
2012-03-18 00:14:16 -0400

    Renamed start to _start.

--- main.asm
+++ main.asm
@@ -6,11 +6,11 @@ segment .data
 	extern	tt_eof, tt_num, tt_alpha, tt_delim, nil
 
 segment .text
-	global start
+	global _start
 	extern _putchar, _flush, _init_strings, _init, \
 		_scan, _putexp, _getexp, _exec, _getexplist
 
-start:
+_start:
 	call	_init_strings
 	call	_init
 	call	_scan
