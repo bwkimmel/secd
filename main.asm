ed87bc40a646e99a4d9746ff535afc7dc01f5d0f
Brad Kimmel <intrepidca@gmail.com>
2012-03-28 22:44:07 -0400

    Added comments to main.asm.

--- main.asm
+++ main.asm
@@ -1,3 +1,6 @@
+; ==============================================================================
+; SECD machine entry point
+; ==============================================================================
 %include 'system.inc'
 
 %define NEWLINE 10
@@ -10,25 +13,28 @@ segment .text
 	extern _putchar, _flush, _init_strings, _init, \
 		_scan, _putexp, _getexp, _exec, _getexplist
 
+; ------------------------------------------------------------------------------
+; Entry point
+;
 _start:
-	call	_init_strings
-	call	_init
+	call	_init_strings		; initialize string-store
+	call	_init				; initialize SECD machine
 	call	_scan
-	call	_getexp
+	call	_getexp				; read SECD object to process
 	mov		ebx, eax
-	call	_getexplist
+	call	_getexplist			; read expressions to pass to entry function
 	push	eax
 	push	ebx
-	call	_exec
+	call	_exec				; pass control to SECD machine
 	add		esp, 8
 	push	eax
-	call	_putexp
+	call	_putexp				; print resulting expression
 	add		esp, 4
 	push	dword NEWLINE
-	call	_putchar	
+	call	_putchar			; print newline
 	add		esp, 4
-	call	_flush
-	sys.exit 0
+	call	_flush				; flush stdout
+	sys.exit 0					; done
 .halt:
 	jmp		.halt
 
