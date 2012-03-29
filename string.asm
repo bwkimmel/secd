1e7928308e12b00bfb891d17b291eeaba6d1cf93
Brad Kimmel <intrepidca@gmail.com>
2012-03-28 23:06:19 -0400

    Added comments to string.asm.

--- string.asm
+++ string.asm
@@ -1,15 +1,30 @@
-%define STORE_SIZE 65536
-%define HASH_SIZE 16381
+; ==============================================================================
+; String-store implementation
+;
+; The string-store represents a set of strings used for symbols.  It ensures
+; that only one copy of any given string is ever added, so that equal strings in
+; the input evaluate to the same symbol.
+; ==============================================================================
+;
+%define STORE_SIZE 65536			; Amount of memory for string storage
+%define HASH_SIZE 16381				; Size of hash table (should be a prime number)
 
 segment .bss
-hash		resd		HASH_SIZE
-data		resb		STORE_SIZE
-dataptr		resd		1
-dataend		resd		1
+hash		resd		HASH_SIZE	; Hash table
+data		resb		STORE_SIZE	; String storage block
+dataptr		resd		1			; Location to store next string
+dataend		resd		1			; End of string stoage block
 
+
+; ==============================================================================
+; Exported functions
+;
 segment .text
 	global	_init_strings, _store
 
+; ------------------------------------------------------------------------------
+; Initializes the string store
+; ------------------------------------------------------------------------------
 _init_strings:
 	enter	0, 0
 	push	edi
@@ -26,44 +41,31 @@ _init_strings:
 	leave
 	ret
 
-_hash:
-	enter	0, 0
-	push	esi
-	mov		esi, [ebp + 8]
-	mov		ecx, [ebp + 12]
-	mov		eax, 0
-.loop:
-		mov		edx, 0
-		mov		dl, byte [esi]
-		inc		esi
-		sub		edx, eax
-		shl		eax, 6
-		add		edx, eax
-		shl		eax, 10
-		add		eax, edx
-		loop	.loop
-.done:
-	pop		esi
-	leave
-	ret
-
+; ------------------------------------------------------------------------------
+; Addes a new string to the string store
+; USAGE: _store(<string>, <length>)
+; <string> = the string to store
+; <length> = the length of the string
+; RETURNS the pointer to the string in the string store, or 0 if the string
+;         store is full.
+; ------------------------------------------------------------------------------
 _store:
 	enter	0, 0
 	push	esi
 	push	edi
 
-	mov		esi, [ebp + 8]
-	mov		ecx, [ebp + 12]
+	mov		esi, [ebp + 8]			; ESI <-- <string>
+	mov		ecx, [ebp + 12]			; ECX <-- <length>
 
 	push	ecx
 	push	esi
-	call	_hash
+	call	_hash					; compute hash code
 	add		esp, 8
 
 	mov		ecx, dword HASH_SIZE
 	mov		edx, 0
 	div		ecx
-.probe_loop:
+.probe_loop:						; scan for empty cell to store string in
 		mov		edi, [dword hash + edx * 4]
 		cmp		edi, 0
 		je		.probe_endloop
@@ -100,8 +102,8 @@ _store:
 
 	mov		ecx, [ebp + 12]
 
-	mov		edi, [dataptr]
-	mov		eax, edi
+	mov		edi, [dataptr]			; check if write will go past end of string
+	mov		eax, edi				; store.
 	add		eax, ecx
 	cmp		eax, [dataend]
 	jge		.full
@@ -109,10 +111,10 @@ _store:
 	mov		[dword hash + edx * 4], edi	
 
 	cld
-	rep		movsb
+	rep		movsb					; write string to string store
 	mov		byte [edi], 0
 	inc		edi
-	xchg	edi, [dataptr]
+	xchg	edi, [dataptr]			; update ptr to next string
 	mov		eax, edi
 .done:
 	pop		edi
@@ -122,4 +124,37 @@ _store:
 .full:
 	mov		eax, 0
 	jmp		.done
+
+
+; ==============================================================================
+; Internal functions
+;
+
+; ------------------------------------------------------------------------------
+; Computes the hash code for a string
+; USAGE: _hash(<string>, <length>)
+; <string> = the string to compute the hash code for
+; <length> = the length of the string
+; RETURNS the hash code
+; ------------------------------------------------------------------------------
+_hash:
+	enter	0, 0
+	push	esi
+	mov		esi, [ebp + 8]			; ESI <-- <string>
+	mov		ecx, [ebp + 12]			; ECX <-- <length>
+	mov		eax, 0
+.loop:
+		mov		edx, 0
+		mov		dl, byte [esi]
+		inc		esi
+		sub		edx, eax
+		shl		eax, 6
+		add		edx, eax
+		shl		eax, 10
+		add		eax, edx
+		loop	.loop
+.done:
+	pop		esi
+	leave
+	ret
 
